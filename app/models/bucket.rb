d38bab9664bb928c64b5c69ee04f957e77218a32
Jeffrey Hardy <jeff@37signals.com>
2024-10-07 09:03:17 -0400

    Scope tags by bucket

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -5,6 +5,7 @@ class Bucket < ApplicationRecord
   belongs_to :creator, class_name: "User", default: -> { Current.user }
 
   has_many :bubbles, dependent: :destroy
+  has_many :tags, -> { distinct }, through: :bubbles
 
   validates_presence_of :name
 end
